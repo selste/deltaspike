7526469559d2cbeed10c11ec83f0742964ac305e
Steffen Elste <selste@gmx.net>
2016-03-04 14:20:54 +0100

    Initial project import

---
+++ deltaspike-windowscope/readme.md
@@ -0,0 +1,7 @@
+deltaspike-windowscope
+======================
+
+Based on the DeltaSpikeWindowScope sample by 'AnghelLeonard' found at
+https://github.com/AnghelLeonard/JSF-2.x/tree/master/DeltaSpikeWindowScope
+
+Modified to run on a Java EE 6 Web Profile (or Full Profile) container like TomEE or WebSphere Liberty
---
+++ deltaspike-windowscope/src/main/java/beansds/PlayerBean.java
@@ -0,0 +1,29 @@
+package beansds;
+
+import java.io.Serializable;
+
+import javax.inject.Named;
+
+import org.apache.deltaspike.core.api.scope.WindowScoped;
+
+@Named
+@WindowScoped
+public class PlayerBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+    private String name = "-";
+
+    public PlayerBean() {
+    	// nothing to do
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }            
+	
+}
